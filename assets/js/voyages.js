ca5d90552190b730c3ac210637b5474f15c08f1b
Telath <clement.pouillart@gmail.com>
2023-01-12 02:07:07 +0100

    Ajout d'une page voyage avec un carousel

---
+++ assets/js/voyages.js
@@ -0,0 +1,15 @@
+/* inspiration 
+https://cz.pinterest.com/pin/830703093774129160/ */
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      }
+    }
+  });
